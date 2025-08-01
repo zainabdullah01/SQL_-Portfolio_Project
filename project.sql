c0a9828499edcedf47ef32015e921bc9b513e44f
Sajid Ejaz <ejazsajid266@gmail.com>
2025-08-01 18:26:52 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -39,22 +39,6 @@ SELECT * FROM Customers;
 SELECT * FROM Orders;
 
 
--- Import Data into Books Table
-COPY Books(Book_ID, Title, Author, Genre, Published_Year, Price, Stock) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Books.csv' 
-CSV HEADER;
-
--- Import Data into Customers Table
-COPY Customers(Customer_ID, Name, Email, Phone, City, Country) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Customers.csv' 
-CSV HEADER;
-
--- Import Data into Orders Table
-COPY Orders(Order_ID, Customer_ID, Book_ID, Order_Date, Quantity, Total_Amount) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Orders.csv' 
-CSV HEADER;
-
-
 -- 1) Retrieve all books in the "Fiction" genre:
 
 select * from books
